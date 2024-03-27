ad89a6390db90946f194212e9918d7ceba9e401f
ivanovdale <dania-spb@mail.ru>
2024-03-27 18:51:50 +0300

    remove unused code

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,19 +1,3 @@
 import Foundation
 
-// MARK: - Use case
-
 let client = HTTPClient()
-
-// MARK: - Get currency data
-
-let cbrApi = CBRApi(client: client)
-let currentDate = Date.getCurrentDate()
-cbrApi.getCurrencyExchangeList(dateString: currentDate)
-cbrApi.getFakeResource()
-cbrApi.getHostNotFound()
-cbrApi.getCurrencyExchangeListWithBadParameter()
-
-// MARK: - Get marvel data
-
-let marvelApi = MarvelApi(client: client)
-marvelApi.getComicsOfCharacter(id: "1011424")
