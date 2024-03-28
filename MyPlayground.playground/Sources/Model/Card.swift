8673498354716ebb776fea0a521f93cb0fe6bbc9
ivanovdale <dania-spb@mail.ru>
2024-03-28 15:07:03 +0300

    add card model

---
+++ MyPlayground.playground/Sources/Model/Card.swift
@@ -0,0 +1,35 @@
+import Foundation
+
+public struct Cards: Decodable {
+    let cards: [Card]
+}
+
+public struct Card: Decodable {
+    let name: String
+    let manaCost: String?
+    let type: String
+    let rarity: String
+    let setName: String
+    let originalText: String?
+}
+
+extension Cards: CustomStringConvertible {
+    public var description: String {
+        cards.map { $0.description }.joined(separator: "\n")
+    }
+}
+
+extension Card: CustomStringConvertible {
+    public var description: String {
+        let text = self.originalText?.isEmpty == false ? "Текст: \(self.originalText!)\n" : ""
+
+        return """
+        Имя карты: \(self.name)
+        Мановая стоимость: \(self.manaCost ?? "Unknown")
+        Тип: \(self.type)
+        Рарность: \(self.rarity)
+        Название сета: \(self.setName)
+        \(text)
+        """
+    }
+}
